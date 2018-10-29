94e6750a150b02e60ec75d8fc4420aa6208f541f
Avner Khan <avnerkhan@Avners-MacBook-Pro.local>
2018-10-28 21:46:36 -0500

    More modular

--- scraper.py
+++ scraper.py
@@ -1,8 +1,24 @@
 import bs4 as bs
+import sqlite3 as db
 import urllib.request as request
 
-raw_data = request.urlopen('https://pythonprogramming.net/').read()
-better_data = bs.BeautifulSoup(raw_data, 'lxml')
+class Aggregator:
+    def __init__(self, target):
+        self.target = target
+    def aggregate(self):
+        raw_data = request.urlopen(self.target).read()
+        better_data = bs.BeautifulSoup(raw_data, 'lxml')
+        return better_data
 
 
-print(better_data.title.text)
+this_item = Aggregator('https://www.w3schools.com/python/python_classes.asp')
+print_this = this_item.aggregate()
+print(print_this)
+
+
+
+
+
+
+
+
